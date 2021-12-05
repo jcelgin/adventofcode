9c144f9fdbd414f9b9c0f4f88e82b9dab7dfdcf3
Chris Elgin <jcelgin@gmail.com>
2021-12-05 10:50:49 -0600

    Working 1

--- 2021/5/csharp/Part1/Program.cs
+++ 2021/5/csharp/Part1/Program.cs
@@ -1,4 +1,4 @@
-﻿using Part1;
+﻿using System.Text.RegularExpressions;
 
 if (args.Length != 1)
 {
@@ -12,4 +12,80 @@ if (!File.Exists(filePath))
     throw new FileNotFoundException(filePath);
 }
 
-var lines = (await File.ReadAllLinesAsync(filePath)).ToArray();
+var lines = await File.ReadAllLinesAsync(filePath);
+
+var r = new Regex("(\\d*,\\d*)", RegexOptions.Compiled);
+
+var map = new Dictionary<Coorindate, int>();
+
+foreach(var line in lines)
+{
+    var m = r.Matches(line);
+    if (m.Count != 2)
+    {
+        throw new Exception($"Unexpected input `{line}`");
+    }
+
+    var c0 = new Coorindate(m[0].Value);
+    var c1 = new Coorindate(m[1].Value);
+
+    Coorindate[] markedCoords;
+
+    if (c0.X == c1.X)
+    {
+        var length = c1.Y - c0.Y;
+        if (length < 0)
+        {
+            var cTemp = c0;
+            c0 = c1;
+            c1 = cTemp;
+            length *= -1;
+        }
+
+        markedCoords = new Coorindate[length+1];
+        for(var i = 0; i <= length; i++)
+        {
+            markedCoords[i] = new Coorindate(c0.X, c0.Y + i);
+        }
+    }
+    else if (c0.Y == c1.Y)
+    {
+        var length = c1.X - c0.X;
+        if (length < 0)
+        {
+            var cTemp = c0;
+            c0 = c1;
+            c1 = cTemp;
+            length *= -1;
+        }
+
+        markedCoords = new Coorindate[length+1];
+        for (var i = 0; i <= length; i++)
+        {
+            markedCoords[i] = new Coorindate(c0.X + i, c0.Y);
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Skipping diagonal `{line}`");
+        continue;
+    }
+
+    foreach (var c in markedCoords)
+    {
+        if (!map.ContainsKey(c))
+        {
+            map.Add(c, 1);
+        }
+        else
+        {
+            map[c]++;
+        }
+    }
+}
+
+var result = map.Aggregate(0, (sum, entry) =>
+    entry.Value > 1 ? sum+1 : sum
+);
+
+Console.WriteLine($"Dangerous positions: {result}");
