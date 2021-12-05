a6a354c7802ae522bc4f12589ff30a67b261bc4a
Chris Elgin <jcelgin@gmail.com>
2021-12-05 08:15:45 -0600

    Setup

---
+++ 2021/5/csharp/Part2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Part1;
+
+if (args.Length != 1)
+{
+    throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
+}
+
+string filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException(filePath);
+}
+
+var lines = (await File.ReadAllLinesAsync(filePath)).ToArray();
