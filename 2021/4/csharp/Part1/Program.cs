9936fe4c9d4a4258fd310af3fd45da4b19f98ea3
Chris Elgin <jcelgin@gmail.com>
2021-12-04 20:28:04 -0600

    Part 1

--- 2021/4/csharp/Part1/Program.cs
+++ 2021/4/csharp/Part1/Program.cs
@@ -1,4 +1,6 @@
-﻿if (args.Length != 1)
+﻿using Part1;
+
+if (args.Length != 1)
 {
     throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
 }
@@ -10,4 +12,43 @@ if (!File.Exists(filePath))
     throw new FileNotFoundException(filePath);
 }
 
-var lines = await File.ReadAllLinesAsync(filePath);
+var lines = (await File.ReadAllLinesAsync(filePath)).ToArray();
+const int boardDimensions = 5;
+
+var numbers = lines[0].Split(',').Select(int.Parse);
+var numboards = (lines.Length - 1) / (boardDimensions+1);
+
+var boards = new BingoBoard[numboards];
+
+for (var i = 0; i < numboards; i++)
+{
+    var boardLines = lines.Skip(1 + (i * (boardDimensions + 1)) + 1).Take(boardDimensions).ToArray(); ;
+    int[,] boardSquares = new int[boardDimensions, boardDimensions];
+    for(var j = 0; j < boardDimensions; j++)
+    {
+        var lineValues = boardLines[j].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        for (var k = 0; k < boardDimensions; k++)
+        {
+            boardSquares[j, k] = int.Parse(lineValues[k]);
+        }
+    }
+    boards[i] = new BingoBoard(boardSquares, boardDimensions);
+}
+
+foreach(var number in numbers)
+{
+    foreach(var board in boards)
+    {
+        if (board.MarkNumber(number))
+        {
+            Console.WriteLine(board);
+
+            var sum = board.GetSumUnmarkedSqures();
+
+            Console.WriteLine($"bingo on {number}! sum {sum}, product {sum*number}");
+            return;
+        }
+    }
+}
+
+Console.WriteLine("No winners found.");
