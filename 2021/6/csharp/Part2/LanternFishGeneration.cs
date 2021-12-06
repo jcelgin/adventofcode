fc86c1a9a8fad451014dce077828fc3026c5760c
Chris Elgin <jcelgin@gmail.com>
2021-12-06 15:53:12 -0600

    Part 2

---
+++ 2021/6/csharp/Part2/LanternFishGeneration.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+
+
+
+
+// var result = generations.Aggregate(0, (sum, gen) => sum + gen.PopulationSize);
+
+internal record LanternFishGeneration
+{
+    public Int64 PopulationSize { get; private set; }
+    public int DaysUntilReproduction { get; private set; }
+
+    public LanternFishGeneration(Int64 populationSize, int daysUntilReproduction)
+    {
+        PopulationSize = populationSize;
+        DaysUntilReproduction = daysUntilReproduction;
+    }
+
+    public bool AdvanceDayAnd()
+    {
+        if (DaysUntilReproduction == 0)
+        {
+            DaysUntilReproduction = 6;
+            return true;
+        }
+        DaysUntilReproduction--;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return $"{DaysUntilReproduction}, {PopulationSize} fish";
+    }
+}
--- 2021/6/csharp/Part2/Program.cs
+++ 2021/6/csharp/Part2/Program.cs
@@ -12,6 +12,36 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
+if (lines.Length != 1)
+{
+    throw new Exception("Expected exactly one line from input file.");
+}
 
-Console.WriteLine($"Result: {result}");
+var generations = lines.First()
+    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .GroupBy(x => x)
+    .Select(x => new LanternFishGeneration(x.Count(), x.Key))
+    .ToList();
+
+const int numDays = 256;
+for (var i = 0; i < numDays; i++)
+{
+    Int64 numNewFish = 0;
+    foreach(var g in generations)
+    {
+        if (g.AdvanceDayAnd())
+        {
+            numNewFish += g.PopulationSize;
+        }
+    }
+
+    if (numNewFish != 0)
+    {
+        generations.Add(new LanternFishGeneration(numNewFish, 8));
+    }
+}
+
+var result = generations.Sum(x => x.PopulationSize);
+
+Console.WriteLine($"Total fish after {numDays}: {result}");
