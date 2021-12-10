8b7a30aee09084522ac10c015ae7bf7117508698
Chris Elgin <jcelgin@gmail.com>
2021-12-09 18:45:57 -0600

    part 2

--- 2021/7/csharp/Part2/Program.cs
+++ 2021/7/csharp/Part2/Program.cs
@@ -3,7 +3,7 @@
     throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
 }
 
-string filePath = args[0];
+var filePath = args[0];
 
 if (!File.Exists(filePath))
 {
@@ -12,6 +12,25 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
+var values = lines.Single().Split(',').Select(int.Parse).ToArray();
 
-Console.WriteLine($"Result: {result}");
+var min = values.Min();
+var max = values.Max();
+
+double minFuel = int.MaxValue;
+
+for (var i = min; i <= max; i++)
+{
+    var k = values.Sum(x =>
+    {
+        var n = Math.Abs(x - i);
+        var r = n / 2.0 * (n + 1);
+        return r;
+    });
+    if (k < minFuel)
+    {
+        minFuel = k;
+    }
+}
+
+Console.WriteLine($"Result: {minFuel}");
