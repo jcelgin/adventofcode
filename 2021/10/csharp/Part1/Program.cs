f355a285dedb030529180f23054b531019038425
Chris Elgin <jcelgin@gmail.com>
2021-12-26 16:24:04 -0700

    Part 1

---
+++ 2021/10/csharp/Part1/Program.cs
@@ -0,0 +1,80 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
+}
+
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException(filePath);
+}
+
+var lines = await File.ReadAllLinesAsync(filePath);
+
+var illegalChars = new List<char>();
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+    var stack = new Stack<char>();
+    var escape = false;
+    foreach (var token in line.ToCharArray())
+    {
+        switch (token)
+        {
+            case '(':
+                stack.Push(')');
+                break;
+            case '[':
+                stack.Push(']');
+                break;
+            case '{':
+                stack.Push('}');
+                break;
+            case '<':
+                stack.Push('>');
+                break;
+            case ')':
+            case ']':
+            case '}':
+            case '>':
+                var p = stack.Pop();
+                if (p != token)
+                {
+                    illegalChars.Add(token);
+                    Console.WriteLine($"\tExpected {p}, but found {token} instead");
+                    escape = true;
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("adsf");
+        }
+
+        if (escape)
+        {
+            break;
+        }
+    }
+    if (!escape)
+    {
+        Console.WriteLine($"\tStack height: {stack.Count}");
+    }
+}
+
+var result = 0;
+
+foreach (var ic in illegalChars.GroupBy(x => x))
+{
+    var multiplier = 0;
+    multiplier = ic.Key switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => throw new ArgumentOutOfRangeException("adsf"),
+    };
+    result += ic.Count() * multiplier;
+}
+
+Console.WriteLine($"Result: {result}");
