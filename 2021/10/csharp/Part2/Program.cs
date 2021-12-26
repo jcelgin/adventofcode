22bf67dade295bd3332ced71024f5def826e83bc
Chris Elgin <jcelgin@gmail.com>
2021-12-26 16:57:44 -0700

    Part 2 init

--- 2021/10/csharp/Part2/Program.cs
+++ 2021/10/csharp/Part2/Program.cs
@@ -3,7 +3,7 @@
     throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
 }
 
-string filePath = args[0];
+var filePath = args[0];
 
 if (!File.Exists(filePath))
 {
@@ -12,6 +12,69 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
+var illegalChars = new List<char>();
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+    var stack = new Stack<char>();
+    var corrupted = false;
+    foreach (var token in line.ToCharArray())
+    {
+        switch (token)
+        {
+            case '(':
+                stack.Push(')');
+                break;
+            case '[':
+                stack.Push(']');
+                break;
+            case '{':
+                stack.Push('}');
+                break;
+            case '<':
+                stack.Push('>');
+                break;
+            case ')':
+            case ']':
+            case '}':
+            case '>':
+                var p = stack.Pop();
+                if (p != token)
+                {
+                    illegalChars.Add(token);
+                    Console.WriteLine($"\tExpected {p}, but found {token} instead");
+                    corrupted = true;
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("adsf");
+        }
+
+        if (corrupted)
+        {
+            break;
+        }
+    }
+    if (!corrupted)
+    {
+        Console.WriteLine($"\tStack height: {stack.Count}");
+    }
+}
+
+var result = 0;
+
+foreach (var ic in illegalChars.GroupBy(x => x))
+{
+    var multiplier = 0;
+    multiplier = ic.Key switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => throw new ArgumentOutOfRangeException("adsf"),
+    };
+    result += ic.Count() * multiplier;
+}
 
 Console.WriteLine($"Result: {result}");
