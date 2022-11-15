f46cf5f0bc2c11c70cf6160f632cecff55db1e2c
Chris Elgin <jcelgin@gmail.com>
2022-11-15 09:06:57 -0600

    parts 1 and 2

--- 2021/11/csharp/Part1/Program.cs
+++ 2021/11/csharp/Part1/Program.cs
@@ -10,9 +10,108 @@ if (!File.Exists(filePath))
     throw new FileNotFoundException(filePath);
 }
 
+const int gridSize = 10;
+
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
-var result = "TODO";
+if (lines.Length != gridSize)
+{
+    throw new ApplicationException($"Expected {gridSize} lines.");
+}
 
-Console.WriteLine($"Result: {result}");
+var grid = new int[gridSize, gridSize];
+
+for (var i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+    if (line.Length != gridSize)
+    {
+        throw new ApplicationException($"Expected lines with length {gridSize}.");
+    }
+
+    for (var j = 0; j < gridSize; j++)
+    {
+        grid[i, j] = (int)char.GetNumericValue(line[j]);
+    }
+}
+
+const int numSteps = 100;
+
+var numFlashes = 0;
+
+void Display()
+{
+    for (var i = 0; i < gridSize; i++)
+    {
+        for (var j = 0; j < gridSize; j++)
+        {
+            Console.Write(grid[i,j]);
+        }
+        Console.Write(Environment.NewLine);
+    }
+    Console.WriteLine($"NumFlashes: {numFlashes}");
+}
+
+void Increment(int i, int j)
+{
+    if (i < 0 || j < 0)
+    {
+        return;
+    }
+
+    if (i >= gridSize || j >= gridSize)
+    {
+        return;
+    }
+
+    if (grid[i, j] == -1)
+    {
+        return;
+    }
+
+    if (grid[i, j] == 9)
+    {
+        grid[i, j] = -1;
+        numFlashes++;
+
+        Increment(i-1, j-1);
+        Increment(i-1, j);
+        Increment(i-1, j+1);
+        Increment(i, j - 1);
+        Increment(i, j + 1);
+        Increment(i+1, j - 1);
+        Increment(i+1, j);
+        Increment(i+1, j + 1);
+    }
+    else
+    {
+        grid[i, j]++;
+    }
+}
+
+Display();
+
+for (var step = 0; step < numSteps; step++)
+{
+    for (var i = 0; i < gridSize; i++)
+    {
+        for (var j = 0; j < gridSize; j++)
+        {
+            Increment(i, j);
+        }
+    }
+
+    for (var i = 0; i < gridSize; i++)
+    {
+        for (var j = 0; j < gridSize; j++)
+        {
+            if (grid[i, j] == -1)
+            {
+                grid[i, j] = 0;
+            }
+        }
+    }
+    Display();
+}
+
+Console.WriteLine($"Result: {numFlashes}");
