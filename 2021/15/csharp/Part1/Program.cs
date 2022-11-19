af2a55571fe3b9ee371a9704fa7df394fe21e871
Chris Elgin <jcelgin@gmail.com>
2022-11-19 14:02:34 -0600

    Part 1 done

--- 2021/15/csharp/Part1/Program.cs
+++ 2021/15/csharp/Part1/Program.cs
@@ -47,13 +47,18 @@ var initialPath = new Path();
 initialPath.Advance(new Tuple<int, int>(0, 0), grid[0, 0]);
 
 var paths = new HashSet<Path> { initialPath };
+var visited = new HashSet<Tuple<int, int>> { initialPath.Position };
 
 var sw = Stopwatch.StartNew();
 while (true)
 {
     // find shortest path, and advance it
-    var shortestPath = paths.OrderBy(x => x.Weight).First();
+    var shortestPath = paths.GroupBy(x => x.Weight)
+        .OrderBy(x => x.Key).First() // shortest weight
+        .OrderByDescending(x => x.Position.Item1 + x.Position.Item2).First(); // nearest the exit.... ish
     paths.Remove(shortestPath);
+
+    Console.WriteLine($"Weight {shortestPath.Weight}, pos {shortestPath.Position.Item1}, {shortestPath.Position.Item2}");
 
     var candidates = new List<Tuple<int, int>>
     {
@@ -76,12 +81,14 @@ while (true)
         try
         {
             var candidateWeight = grid[candidate.Item1, candidate.Item2];
+            if (!visited.Add(candidate))
+            {
+                // we've already been here, and it was cheaper
+                continue;
+            }
             var newPath = new Path { Position = shortestPath.Position, Weight = shortestPath.Weight };
             newPath.Advance(candidate, candidateWeight);
-            if (!paths.Add(newPath))
-            {
-                ;
-            }
+            paths.Add(newPath);
         }
         catch (IndexOutOfRangeException)
         {
