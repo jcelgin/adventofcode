71aa5cf33ea9d4a8dc0a94434bbcfa02c9b23ed2
Chris Elgin <jcelgin@gmail.com>
2022-11-18 19:14:55 -0600

    Progress, before I rewrite this whole thing

--- 2021/14/csharp/Part2/Program.cs
+++ 2021/14/csharp/Part2/Program.cs
@@ -1,4 +1,23 @@
-﻿if (args.Length != 1)
+﻿using System.Diagnostics;
+using System.Text;
+
+string Transform(string s, IReadOnlyDictionary<string, char[]> mutations)
+{
+    var sb = new StringBuilder();
+
+    for (var i = 0; i < s.Length-1; i++)
+    {
+        var pattern = s.Substring(i, 2);
+        var result = mutations[pattern];
+        sb.Append(result);
+    }
+
+    sb.Append(s.Last());
+
+    return sb.ToString();
+}
+
+if (args.Length != 1)
 {
     throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
 }
@@ -12,7 +31,31 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
-var result = "TODO";
+var pattern = lines[0];
 
-Console.WriteLine($"Result: {result}");
+var mutations = new Dictionary<string, char[]>();
+for (var i = 2; i < lines.Length; i++)
+{
+    var tokens = lines[i].Split(" -> ");
+    var value = new[] { tokens[0][0], tokens[1][0] };
+    mutations.Add(tokens[0], value);
+}
+
+const int numTransformations = 10;
+
+var result = pattern;
+
+for (var i = 0; i < numTransformations; i++)
+{
+    var sw = Stopwatch.StartNew();
+    result = Transform(result, mutations);
+    sw.Stop();
+    Console.WriteLine($"Completed step {i} in {sw.ElapsedMilliseconds}ms");
+    //Console.WriteLine($"{i}: {result}");
+}
+
+var k = result.GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() }).OrderBy(x => x.Count).ToArray();
+
+var diff = k.Last().Count - k.First().Count;
+
+Console.WriteLine($"Diff: {diff}");
