22df352e35cbca50ed52d0068f1ac40c3c60c46b
Chris Elgin <jcelgin@gmail.com>
2022-12-01 18:06:06 -0600

    Alts

---
+++ 2022/1/csharp/Part1 - Alt/Program.cs
@@ -0,0 +1,15 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
+}
+
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException(filePath);
+}
+
+var input = await File.ReadAllTextAsync(filePath);
+var elves = input.Split($"{Environment.NewLine}{Environment.NewLine}");
+Console.WriteLine($"Result: {elves.Select(x => x.Split(Environment.NewLine).Sum(int.Parse)).Max()}");
