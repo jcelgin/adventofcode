670a1ebef73a40799545ab5c810c240c58a0f192
Chris Elgin <jcelgin@gmail.com>
2022-12-02 19:06:42 -0600

    p2

--- 2015/2/csharp/Part2/Program.cs
+++ 2015/2/csharp/Part2/Program.cs
@@ -12,7 +12,18 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
-var result = "TODO";
+var total = 0;
+foreach (var line in lines)
+{
+    var tokens = line.Split('x').Select(int.Parse).OrderBy(x => x).ToArray();
 
-Console.WriteLine($"Result: {result}");
+    if (tokens.Length != 3)
+    {
+        throw new ApplicationException("We only deal in three dimensions, sir.");
+    }
+
+    // ReSharper disable once ArrangeRedundantParentheses
+    total += (tokens[0] + tokens[1]) * 2 + (tokens[0] * tokens[1] * tokens[2]);
+}
+
+Console.WriteLine($"Result: {total}");
