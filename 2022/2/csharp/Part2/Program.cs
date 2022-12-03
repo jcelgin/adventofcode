cc3f0f0156ae2506f2338142f5cad500d7a92888
Chris Elgin <jcelgin@gmail.com>
2022-12-02 18:00:29 -0600

    Working p2

--- 2022/2/csharp/Part2/Program.cs
+++ 2022/2/csharp/Part2/Program.cs
@@ -12,7 +12,51 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
-var result = "TODO";
+var myScore = 0;
 
-Console.WriteLine($"Result: {result}");
+foreach (var line in lines)
+{
+    var opMove = line[0];
+    var idealOutcome = line[2];
+
+    var matchScore = 0;
+
+    var myMove = 0;
+    // determine my move
+    switch (idealOutcome)
+    {
+        case 'X':
+            // need to lose
+            myMove = opMove switch
+            {
+                'A' => 'C',
+                'B' => 'A',
+                'C' => 'B',
+                _ => myMove
+            };
+            break;
+        case 'Y':
+            // equal to the other
+            myMove = opMove;
+            matchScore += 3;
+            break;
+        case 'Z':
+            // need to win
+            myMove = opMove switch
+            {
+                'A' => 'B',
+                'B' => 'C',
+                'C' => 'A',
+                _ => myMove
+            };
+            matchScore += 6;
+            break;
+    }
+
+    var score = myMove - 64;
+    matchScore += score;
+
+    myScore += matchScore;
+}
+
+Console.WriteLine($"Result: {myScore}");
