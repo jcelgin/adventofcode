1499ff216ab2ef20ea4e7a7ad6b84ecfb8617fb5
Chris Elgin <jcelgin@gmail.com>
2022-12-06 09:39:34 -0600

    Working but lame p2

--- 2022/4/csharp/Part2/Program.cs
+++ 2022/4/csharp/Part2/Program.cs
@@ -12,7 +12,33 @@ if (!File.Exists(filePath))
 
 var lines = await File.ReadAllLinesAsync(filePath);
 
-// TODO
-var result = "TODO";
+var linesWithOverlap = 0;
 
-Console.WriteLine($"Result: {result}");
+foreach (var line in lines)
+{
+    var elfLoad = line.Split(',')
+        .Select(x => x.Split('-').Select(int.Parse).ToArray())
+        .ToArray();
+
+    var e1 = elfLoad[0];
+    var e2 = elfLoad[1];
+
+    var r1 = Enumerable.Range(e1[0], e1[1] - e1[0] + 1);
+
+    if (r1.Contains(e2[0]) || r1.Contains(e2[1]))
+    {
+        linesWithOverlap++;
+        Console.WriteLine(line);
+        continue;
+    }
+
+    var r2 = Enumerable.Range(e2[0], e2[1] - e2[0] + 1);
+    if (r2.Contains(e1[0]) || r2.Contains(e1[1]))
+    {
+        linesWithOverlap++;
+        Console.WriteLine(line);
+        continue;
+    }
+}
+
+Console.WriteLine($"Result: {linesWithOverlap}");
