8da4de76b27520eb798771cc226385e15b32dfef
Chris Elgin <jcelgin@gmail.com>
2022-12-05 19:26:51 -0600

    Day3

---
+++ 2022/3/csharp/Part1/Program.cs
@@ -0,0 +1,34 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
+}
+
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException(filePath);
+}
+
+var lines = await File.ReadAllLinesAsync(filePath);
+var totalPriority = 0;
+
+foreach (var line in lines)
+{
+    var back = line[(line.Length / 2)..].ToHashSet();
+
+    var k = line.Length / 2;
+    for (var i = 0; i < k; i++)
+    {
+        if (back.Contains(line[i]))
+        {
+            var val = line[i] > 90 ? line[i] - 96 : line[i] - 38;
+
+            totalPriority += val;
+            Console.WriteLine($"Common: {line[i]}, {val}");
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Result: {totalPriority}");
---
+++ 2022/3/csharp/Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿int GetPriorityOf(char a)
+{
+    var val1 = a > 90 ? a - 96 : a - 38;
+    return val1;
+}
+
+if (args.Length != 1)
+{
+    throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
+}
+
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException(filePath);
+}
+
+var lines = await File.ReadAllLinesAsync(filePath);
+
+const int chunkSize = 3;
+
+var totalPriority = lines.Chunk(chunkSize)
+    .Select(chunk => chunk[0].Intersect(chunk[1]).Intersect(chunk[2]))
+    .Select(intersection => GetPriorityOf(intersection.Single()))
+    .Sum();
+
+Console.WriteLine($"Result: {totalPriority}");
