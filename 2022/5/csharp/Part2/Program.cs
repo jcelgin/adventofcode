d980b90345db44a4254f4bc36074c702a42c0b2c
Chris Elgin <jcelgin@gmail.com>
2022-12-06 12:32:33 -0600

    Part 2

--- 2022/5/csharp/Part2/Program.cs
+++ 2022/5/csharp/Part2/Program.cs
@@ -1,4 +1,6 @@
-﻿if (args.Length != 1)
+﻿using System.Text;
+
+if (args.Length != 1)
 {
     throw new ArgumentException($"Expected 1 argument (file path), got {args.Length}");
 }
@@ -10,9 +12,62 @@ if (!File.Exists(filePath))
     throw new FileNotFoundException(filePath);
 }
 
-var lines = await File.ReadAllLinesAsync(filePath);
+var allText = await File.ReadAllTextAsync(filePath);
 
-// TODO
-var result = "TODO";
+var allTextSplit = allText.Split($"{Environment.NewLine}{Environment.NewLine}").Select(x => x.Split(Environment.NewLine)).ToArray();
+
+// parse state
+var state = allTextSplit[0];
+var numStacks = state.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Max();
+
+if (numStacks > 9)
+{
+    throw new ApplicationException("All this breaks if there are double digit stack numbers");
+}
+
+var stacks = new Stack<char>[numStacks];
+
+for (var i = 0; i < numStacks; i++)
+{
+    var myStack = new Stack<char>();
+    var myOffset = 1 + (i * 4);
+    for (var j = state.Length - 2; j >= 0; j--)
+    {
+        var value = state[j][myOffset];
+        if (value != ' ')
+        {
+            myStack.Push(value);
+        }
+    }
+
+    stacks[i] = myStack;
+}
+
+// directions
+var directions = allTextSplit[1];
+foreach (var line in directions)
+{
+    var tokens = line.Split(' ');
+    var num = int.Parse(tokens[1]);
+    var source = int.Parse(tokens[3]) - 1;
+    var target = int.Parse(tokens[5]) - 1;
+
+    var sb = new StringBuilder();
+    for (var i = 0; i < num; i++)
+    {
+        sb.Append(stacks[source].Pop());
+    }
+
+    foreach (var c in sb.ToString().Reverse())
+    {
+        stacks[target].Push(c);
+    }
+}
+
+var result = new StringBuilder();
+foreach (var s in stacks)
+{
+    result.Append(s.Peek());
+}
 
 Console.WriteLine($"Result: {result}");
